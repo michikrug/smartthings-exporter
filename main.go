71d7f5f85b173be7ca086cf48ce57ccd47f12632
Michael Krug <michi.krug@gmail.com>
2025-02-08 22:14:57 +0100

    Remove logging flags

--- main.go
+++ main.go
@@ -162,8 +162,6 @@ func checkEnvVars(vars []string) {
 }
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, relying on environment variables")
 	}
